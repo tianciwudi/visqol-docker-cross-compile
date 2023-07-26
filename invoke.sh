fa2d0969cd5ef30b64a0072198137d72ddd6fbbd
NicoSeddiki <nseddiki@sipfront.com>
2023-07-26 11:02:13 +0200

    Updated invoke.sh script -> now the whole build process is fully automated

--- invoke.sh
+++ invoke.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Specify container name
+container_name="sipfront-test"
+
 # Pull docker image and tag it 
 docker pull amazonlinux:2 && docker tag amazonlinux:2 local-amazonlinux:2
 
@@ -5,7 +10,12 @@ docker pull amazonlinux:2 && docker tag amazonlinux:2 local-amazonlinux:2
 docker build --tag="local-amazonlinux:latest" .
 
 # Capture the container ID
-CONTAINER_ID=$(docker run -it local-amazonlinux:latest)
+docker run --name $container_name local-amazonlinux:latest
 
 # Copy visqol build folder to working directory
-docker cp $CONTAINER_ID:/visqol/build/lib/visqol/ ./build/ 
+echo "Copying visqol build directory!"
+docker cp sipfront-test:/visqol/build/lib/visqol/ ./build_test/ 
+
+# Remove used container
+echo "Removing container!" 
+docker rm $container_name
